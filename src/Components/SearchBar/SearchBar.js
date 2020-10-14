86be839a08406eddc05ac27c2af1025e1b80d2a5
Justin <67116123+Essentialjustin@users.noreply.github.com>
2020-10-14 17:40:54 -0600

    Commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+class SearchBar extends React.Component {
+
+    render() {
+      return (
+        <div class="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <button class="SearchButton">SEARCH</button>
+      </div>
+      );
+    }
+  }
+  
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <TrackList /> */}
+      </div>
+    );
+  }
+}
