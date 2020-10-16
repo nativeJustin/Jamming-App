68b2d128b1aaae0335ea25fd2179dfd8c73ec84a
Justin <67116123+Essentialjustin@users.noreply.github.com>
2020-10-16 15:56:41 -0600

    enter

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "../App/App.css";
 
-import Playlist from '../Playlist/Playlist';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
 
-
-class App extends React.Component{
+class App extends React.Component {
   render() {
     return (
       <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
           <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
